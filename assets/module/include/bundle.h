0d4f1b142f715ade199215e5322977dab0a5cd7e
TeamPuzel <puffed-01.blips@icloud.com>
2024-08-20 19:14:35 +0200

    remove accidentally left in generated code

--- assets/module/include/bundle.h
+++
@@ -1,5 +0,0 @@
-const unsigned char *const SHEET_TGA;
-const unsigned char *const KENNEY_TGA;
-const unsigned char *const UI_TGA;
-const unsigned char *const TILES_TGA;
-const unsigned char *const PICOFONT_TGA;
